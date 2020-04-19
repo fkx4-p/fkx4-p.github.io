3afdb21417094f43f4cbaab7af442d55caf36ff4
Ink-33 <quhyu@qq.com>
2020-04-20 03:10:34 +0800

    Fixed avatars loading issues.

--- MDUI-Monitor/js/monitor.js
+++ MDUI-Monitor/js/monitor.js
@@ -12,46 +12,46 @@ let userID = urlParams.get('uid')
 let cloc = urlParams.get('refresh-time')
 let iden = urlParams.get('iden')
 cloc *= 1000
-if(cloc <= 10000 && iden != "FFEE_CO"){
+if (cloc <= 10000 && iden != "FFEE_CO") {
   cloc = 10000
 }
-if(userID.length){
+if (userID.length) {
   localStorage.setItem('userID', userID)
 }
-else if(localStorage.getItem('userID')){
+else if (localStorage.getItem('userID')) {
   userID = localStorage.getItem('userID')
 }
 //MID, Avatar and Fans output to html
-const showFansCount = (json)=>{
-  elFans.innerText = Number( json.data.follower ).toLocaleString()
-  elMid.innerText = Number( json.data.card.mid ).toLocaleString()
+const showFansCount = (json) => {
+  elFans.innerText = Number(json.data.follower).toLocaleString()
+  elMid.innerText = Number(json.data.card.mid).toLocaleString()
   elName.innerText = json.data.card.name.toLocaleString()
-  elAvatar.src = json.data.card.face.replace(/^http:/,'https:')
+  elAvatar.src = json.data.card.face.replace(/^http:/, 'https:')
 }
-function returnAvatar(){
-  elAvatar.src = json.data.card.face.replace(/^http:/,'https:')
+function returnAvatar() {
+  elAvatar.src = json.data.card.face.replace(/^http:/, 'https:')
   return elAvatar.src.toString()
 }
 //get jsonp
 let elScriptChild
-const appendScript = ()=>{
-  const url = "https://json2jsonp.com/?url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D"+userID+'&spam='+Number(new Date)+"&callback=showFansCount"//"https://jsonp.afeld.me/?callback=showFansCount&url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D"+userID+'&spam='+Number(new Date)
+const appendScript = () => {
+  const url = "https://json2jsonp.com/?url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D" + userID + '&spam=' + Number(new Date) + "&callback=showFansCount"//"https://jsonp.afeld.me/?callback=showFansCount&url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D"+userID+'&spam='+Number(new Date)
   elScriptChild = document.createElement('script')
   elScriptChild.setAttribute('src', url)
   elScript.appendChild(elScriptChild)
 }
 appendScript()
 //redo after timeout
-const getData = ()=>{
+const getData = () => {
   elScript.removeChild(elScriptChild)
   appendScript()
 }
 window.setInterval(getData, cloc)
 //font size and stuff
-const resize = ()=>{
-  elFans.style.fontSize = document.getElementById('main').clientWidth*0.03+'px'
-  elName.style.fontSize = document.getElementById('main').clientWidth*0.03+'px'
-  elMid.style.fontSize = document.getElementById('main').clientWidth*0.03+'px'
+const resize = () => {
+  elFans.style.fontSize = document.getElementById('main').clientWidth * 0.03 + 'px'
+  elName.style.fontSize = document.getElementById('main').clientWidth * 0.03 + 'px'
+  elMid.style.fontSize = document.getElementById('main').clientWidth * 0.03 + 'px'
 }
 resize()
 window.onresize = resize
